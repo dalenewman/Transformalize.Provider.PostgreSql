0d0551fac37f74af73d6ba19d2d6bb83bfc3d620
Dale Newman <dalenewman@gmail.com>
2020-04-07 17:12:27 -0400

    added .net standard 2.0 target

--- src/Transformalize.Provider.PostgreSql/Properties/AssemblyInfo.cs
+++ src/Transformalize.Provider.PostgreSql/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Transformalize.Provider.MySql")]
+[assembly: AssemblyTitle("Transformalize.Provider.PostgreSql")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Transformalize")]
-[assembly: AssemblyProduct("Transformalize.Provider.MySql")]
-[assembly: AssemblyCopyright("Copyright © 2018-2019 Dale Newman")]
+[assembly: AssemblyProduct("Transformalize.Provider.PostgreSql")]
+[assembly: AssemblyCopyright("Copyright © 2018-2020 Dale Newman")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.26.0")]
-[assembly: AssemblyFileVersion("0.6.26.0")]
+[assembly: AssemblyVersion("0.6.28.0")]
+[assembly: AssemblyFileVersion("0.6.28.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
