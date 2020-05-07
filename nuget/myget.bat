2b22cf03c6a39bc25cc0884ab862263ed364a3e9
Dale Newman <dalenewman@gmail.com>
2020-05-07 08:38:09 -0400

    bumped version

--- nuget/myget.bat
+++ nuget/myget.bat
@@ -1,8 +1,8 @@
 nuget pack Transformalize.Provider.PostgreSql.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Provider.PostgreSql.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
-nuget push "c:\temp\modules\Transformalize.Provider.PostgreSql.0.7.4-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Provider.PostgreSql.Autofac.0.7.4-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.PostgreSql.0.7.6-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.PostgreSql.Autofac.0.7.6-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
 
 
 
--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -1,8 +1,8 @@
 REM nuget pack Transformalize.Provider.PostgreSql.nuspec -OutputDirectory "c:\temp\modules"
 REM nuget pack Transformalize.Provider.PostgreSql.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
-nuget push "c:\temp\modules\Transformalize.Provider.PostgreSql.0.7.4-beta.nupkg" -source https://api.nuget.org/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Provider.PostgreSql.Autofac.0.7.4-beta.nupkg" -source https://api.nuget.org/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.PostgreSql.0.7.6-beta.nupkg" -source https://api.nuget.org/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.PostgreSql.Autofac.0.7.6-beta.nupkg" -source https://api.nuget.org/v3/index.json
 
 
 
