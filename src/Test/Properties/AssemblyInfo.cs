ed5d9f24b8e8b066e7cee06fe4ddceb49457c5ed
Dale Newman <dalenewman@gmail.com>
2020-07-11 15:28:36 -0400

    remove stale nuget packages

--- src/Test/Properties/AssemblyInfo.cs
+++ src/Test/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("55682ffb-39b0-46a4-be91-934a1a69e7dc")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.8.15.0")]
+[assembly: AssemblyFileVersion("0.8.15.0")]
