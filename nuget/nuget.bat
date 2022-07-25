7ba9ff16615ba056dd3f1b5f687391a66c3f90cf
dalenewman <dalenewman@gmail.com>
2022-07-25 17:35:55 -0400

    update dependency on ado library

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -1,5 +1,5 @@
 nuget pack Transformalize.Provider.PostgreSql.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Provider.PostgreSql.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
-REM nuget push "c:\temp\modules\Transformalize.Provider.PostgreSql.0.10.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.PostgreSql.Autofac.0.10.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.PostgreSql.0.10.2-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.PostgreSql.Autofac.0.10.2-beta.nupkg" -source https://api.nuget.org/v3/index.json
