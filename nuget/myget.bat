b22852a7b6a2fa5f58e7cbbd7f44ea2d352e3ea0
Dale Newman <dalenewman@gmail.com>
2025-04-28 22:03:44 -0400

    bump ado dependency for some fixes

--- nuget/myget.bat
+++ nuget/myget.bat
@@ -1,5 +1,5 @@
 nuget pack Transformalize.Provider.PostgreSql.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Provider.PostgreSql.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
-nuget push "c:\temp\modules\Transformalize.Provider.PostgreSql.0.11.2-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Provider.PostgreSql.Autofac.0.11.2-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.PostgreSql.0.11.4-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.PostgreSql.Autofac.0.11.4-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
